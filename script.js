65aac9548fdd9037086571d7347ddb836bd4cb18
James Harding <99635678+JaHa675@users.noreply.github.com>
2024-04-06 17:09:10 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+console.log("loaded on the html")
+
+const todoList = document.querySelector("#todo_list")
+//TODO: fill pokemon array
+const pokemonArray = [] 
+//TODO: finish function
+function addTodoItem(){
+    //get info from text field 
+    //use data to create new todo elements
+    //append elements to list
+}
+//TODO: finish function
+function removeTodoItem(){
+    //get todo storage location from TBD
+    //use data to remove todo item from dom node
+    //give a toast that says deleted
+}
+//TODO: finish function
+function initializeEditTodoItem(){
+    //get info about which field needs editing
+    //open textarea to allow user input
+    
+}
+//TODO: finish function
+function finalizeEditTodoItem(){
+    //get info from text field 
+    //use data to update current todo element
+    // return todo item to standard format
+        //removing our textarea
+        //readding p tag
+}
+//TODO:
+function assignPokemon(){
+    //TODO:break this into another function
+    // makes call to poke api for data/checks if data is stored in pokemon array
+
+    // recieve info about pokemon from assignment aside
+    // returns info about pokemon from array to calling function 
+}
